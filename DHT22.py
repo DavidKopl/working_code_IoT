e1c4c10a6e5a94882fbbb07ead9d0085ca18e9b3
David - Raspberry Pi 4 <kopldavid@seznam.cz>
2024-11-22 18:20:42 +0100

    temp save

--- DHT22.py
+++ DHT22.py
@@ -8,7 +8,7 @@ while True:
     vlhkost, teplota = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
     if vlhkost is not None and teplota is not None:
-        print(f"Teplota: {teplota:.1f} C   Vlhkost: {vlhkost:.1f}%")
+        print(f"Teplota: {teplota:.1f} C   Vlhkost: {vlhkost:.1f} %")
     else:
         print("Chyba pri cteni ze senzoru DHT22.")
     time.sleep(2)
