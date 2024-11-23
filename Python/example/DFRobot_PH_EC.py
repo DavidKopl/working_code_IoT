72a0772bf1b49c20c36c90a8322c56feb5bc735e
David - Raspberry Pi 4 <kopldavid@seznam.cz>
2024-11-23 20:36:48 +0100

    all working PH EC

--- Python/example/DFRobot_PH_EC.py
+++ Python/example/DFRobot_PH_EC.py
@@ -16,68 +16,20 @@ ads1115 = ADS1115()
 ec      = DFRobot_EC()
 ph      = DFRobot_PH()
 
-VREF = 5000  # Referenční napětí v mV
-ADC_RES = 1024  # Rozlišení ADC
-TWO_POINT_CALIBRATION = 0
-
-CAL1_V = 1600  # Kalibrační napětí při teplotě CAL1_T (mV)
-CAL1_T = 25    # Kalibrační teplota 1 (°C)
-CAL2_V = 1300  # Kalibrační napětí při teplotě CAL2_T (mV)
-CAL2_T = 15    # Kalibrační teplota 2 (°C)
-
-DO_Table = [
-    14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
-    11260, 11010, 10770, 10530, 10300, 10080, 9860, 9660, 9460, 9270,
-    9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
-    7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410
-]
-
-def readDO(voltage_mv, temperature_c):
-    """Výpočet koncentrace rozpuštěného kyslíku (DO)."""
-    if TWO_POINT_CALIBRATION == 0:
-        V_saturation = CAL1_V + 35 * temperature_c - CAL1_T * 35
-        return (voltage_mv * DO_Table[temperature_c] // V_saturation)
-    else:
-        V_saturation = ((temperature_c - CAL2_T) * (CAL1_V - CAL2_V) // (CAL1_T - CAL2_T)) + CAL2_V
-        return (voltage_mv * DO_Table[temperature_c] // V_saturation)
-
 ec.begin()
 ph.begin()
-while True:
-    # Čtení teploty (předpokládáme konstantní hodnotu teploty)
-    temperature = 25
-
-    # Nastavení adresy ADS1115
-    ads1115.setAddr_ADS1115(0x48)
-
-    # Nastavení zisku pro ADS1115
-    ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
-
-    # Čtení napětí z ADC kanálů
-    adc0 = ads1115.readVoltage(0)  # Napětí z kanálu 0
-    adc1 = ads1115.readVoltage(1)  # Napětí z kanálu 1
-    adc2 = ads1115.readVoltage(2)  # Napětí z kanálu 2 pro DO
-
-    # Výpočet napětí DO
-    # adc_voltage_DO = VREF * adc2 / ADC_RES
-    adc_voltage_DO = VREF * adc2['r'] / ADC_RES
-
-
-    # Výpočet hodnoty DO
-    DO = readDO(adc2, temperature)
-
-    # Výpočet EC a pH
-    EC = ec.readEC(adc0, temperature)
-    PH = ph.readPH(adc1, temperature)
-
-    # Výpis výsledků
-    print(f"Teplota: {temperature} °C, EC: {EC:.2f} ms/cm, PH: {PH:.2f}, DO: {DO:.2f} mg/L")
-
-    # Pauza mezi měřeními
-    time.sleep(1.0)
-
-  
-
-
-
-
+while True :
+	#Read your temperature sensor to execute temperature compensation
+	temperature = 25
+	#Set the IIC address
+	ads1115.setAddr_ADS1115(0x48)
+	#Sets the gain and input voltage range.
+	ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
+	#Get the Digital Value of Analog of selected channel
+	adc0 = ads1115.readVoltage(0)
+	adc1 = ads1115.readVoltage(1)
+	#Convert voltage to EC with temperature compensation
+	EC = ec.readEC(adc0['r'],temperature)
+	PH = ph.readPH(adc1['r'],temperature)
+	print("Temperature:%.1f ^C EC:%.2f ms/cm PH:%.2f " %(temperature,EC,PH))
+	time.sleep(1.0)
